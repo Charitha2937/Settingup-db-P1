ad953bfbb475c202fc95c643732666b81372db01
Charitha2937 <charitharayaluthirumalasetty@gmail.com>
2025-02-24 17:59:54 +0530

    Added files

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const { resolve } = require('path');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = 3010;
 
 app.use(express.static('static'));
+
+
+
+const DB_URL = `mongodb+srv://charitharayaluthirumalasetty:Charitha%4086@practiceassignments.z6dmq.mongodb.net/?retryWrites=true&w=majority&appName=PracticeAssignments`;
+mongoose.connect(DB_URL)
+.then(()=>{console.log('Mongodb connected Successfully!')})
+.catch(err => console.log('Mongodb connection failed : ',err));
+
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
